661b8eff265ad8e44f693f95dd8b6f90ac7cc78b
Moyeong Lee <2000prcs@gmail.com>
2018-07-21 19:48:03 -0700

    Allow users to watch a movie trailer
    - Directs users to the movie trailer when a user clicks the movie poster or the label

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import _ from 'lodash';
 import {
   Table,
@@ -7,7 +7,11 @@ import {
   Image,
   Header,
   Rating,
+  Label,
 } from 'semantic-ui-react';
+
+const { API_KEY } = require('../../config/IMDBconfig');
+
 
 export default class Movies extends React.Component {
   constructor(props) {
@@ -38,6 +42,15 @@ export default class Movies extends React.Component {
     }, 300);
   }
 
+  getMovieTrailer(id) {
+    fetch(`http://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ movieTrailerKey: data.results[0].key });
+      })
+      .catch(error => console.log('Failed', error));
+  }
+
   // Sort the table by each column
   handleSort(clickedColumn) {
     const { column, data, direction } = this.state;
@@ -58,15 +71,9 @@ export default class Movies extends React.Component {
     });
   }
 
-  getMovieTrailer(){
-    let url = `http://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}`
- 
-
-  }
-
 
   render() {
-    const { column, data, direction } = this.state;
+    const { column, data, direction, movieTrailerKey } = this.state;
 
     return (
       <Table sortable fixed singleLine striped selectable>
@@ -99,13 +106,23 @@ export default class Movies extends React.Component {
           {_.map(data, ({ id, title, vote_average, release_date, overview, poster_path }) => (
             <Table.Row key={id}>
               <Table.Cell>
-                <Modal size="small" trigger={<Button basic fluid>{title}</Button>}>
+                <Modal size="small" trigger={<Button onClick={() => this.getMovieTrailer(id)} basic fluid>{title}</Button>}>
                   <Modal.Header>{title}</Modal.Header>
                   <Modal.Content image>
-                    <Image wrapped size="medium" src={`http://image.tmdb.org/t/p/w342${poster_path}`} />
+                    <Image
+                      as="a"
+                      wrapped
+                      size="large"
+                      src={`http://image.tmdb.org/t/p/w342${poster_path}`} 
+                      href={`https://www.youtube.com/watch?v=${movieTrailerKey}`}
+                      target="_blank"
+                    />
                     <Modal.Description>
                       <Header></Header>
                       <p>{overview}</p>
+                      <Label as="a" color="teal" tag href={`https://www.youtube.com/watch?v=${movieTrailerKey}`} target="_blank">
+                        Watch Movie Trailer
+                      </Label>
                     </Modal.Description>
                   </Modal.Content>
                 </Modal>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Input, Icon, Label } from 'semantic-ui-react';
 
 
